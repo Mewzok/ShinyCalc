858da7ee90e16851dd8ad8f1d311f7706de9d7b4
Mewzok <mewzok7@yahoo.com>
2018-09-11 23:46:39 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shinycalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int shinyChance = 8192;
+            int numChosen = 0;
+            int counter = 0;
+
+            Random rnd = new Random();
+
+            while(numChosen != shinyChance)
+            {
+                numChosen = rnd.Next(8193);
+                counter++;
+
+                if (counter % 100 == 0)
+                    Console.WriteLine("Running... " + counter);
+            }
+
+            Console.Write("Number chosen in " + counter + " tries.");
+            Console.ReadLine();
+        }
+    }
+}
